95e9342cbb5582060b39ec70234dc88c8b6d2d10
Brian Granados <bragrji@gmail.com>
2025-03-02 21:23:41 -0600

    feat: Add initial GSAP setup with HTML and JavaScript files

---
+++ GSAP/code.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(Flip, ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin, EaselPlugin, PixiPlugin, TextPlugin, RoughEase, ExpoScaleEase, SlowMo, CustomEase)
+
+});
+
